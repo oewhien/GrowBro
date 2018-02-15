7cf60fc72c318b1aaf5b28a9b7cd258c83bbf180
Robert Meißner <robeme@Roberts-MBP-2.local>
2018-02-15 21:14:14 +0100

    some more comments

--- PumpControl.ino
+++ PumpControl.ino
@@ -2,9 +2,9 @@
 
 // LCD
 #define RS 12
-#define EN 11 
-#define D4 8 
-#define D5 7 
+#define EN 11
+#define D4 8
+#define D5 7
 #define D6 6
 #define D7 5
 #define LEDPN 10
@@ -13,7 +13,7 @@
 #define ANINPN A0
 #define MESVPN 13
 // Button and rotation encoder
-#define PN0 2 
+#define PN0 2
 #define PN1 4
 #define PN2 3 // This is the MuFuK
 
@@ -31,7 +31,7 @@ byte canUl[8] = {
   B00100,
   B00101,
   B00011,
-  B00001,  
+  B00001,
   B01100,
 };
 byte canUr[8] = {
@@ -40,7 +40,7 @@ byte canUr[8] = {
   B11100,
   B11110,
   B11111,
-  B11111,  
+  B11111,
   B11111,
 };
 byte canLl[8] = {
@@ -49,7 +49,7 @@ byte canLl[8] = {
   B00000,
   B01000,
   B00000,
-  B00100,  
+  B00100,
   B10000,
 };
 byte canLr[8] = {
@@ -58,7 +58,7 @@ byte canLr[8] = {
   B00000,
   B00000,
   B00000,
-  B00000,  
+  B00000,
   B00000,
 };
 
@@ -68,7 +68,7 @@ byte flowerUl[8] = {
   B00111,
   B01100,
   B11000,
-  B10010,  
+  B10010,
   B10000,
 };
 byte flowerUr[8] = {
@@ -77,7 +77,7 @@ byte flowerUr[8] = {
   B11100,
   B00110,
   B00011,
-  B01001,  
+  B01001,
   B00001,
 };
 byte flowerLl[8] = {
@@ -86,7 +86,7 @@ byte flowerLl[8] = {
   B11001,
   B01100,
   B00111,
-  B00000,  
+  B00000,
   B00000,
 };
 byte flowerLr[8] = {
@@ -95,7 +95,7 @@ byte flowerLr[8] = {
   B10011,
   B00110,
   B11100,
-  B00000,  
+  B00000,
   B00000,
 };
 
@@ -113,11 +113,11 @@ void isr ()
 
 void isp (){
   if (digitalRead(PN2))
-    pushed = true;  
+    pushed = true;
 }
 
-void setup() {  
-  lcd.begin(16, 2);  
+void setup() {
+  lcd.begin(16, 2);
   lcd.createChar(0,canUl);
   lcd.createChar(1,canUr);
   lcd.createChar(2,canLl);
@@ -126,19 +126,19 @@ void setup() {
   lcd.createChar(5,flowerUr);
   lcd.createChar(6,flowerLl);
   lcd.createChar(7,flowerLr);
-  
-  pinMode(PUMPPN, OUTPUT);  
+
+  pinMode(PUMPPN, OUTPUT);
   pinMode(MESVPN, OUTPUT);
-  
+
   // Rotation encoder
   digitalWrite (PN0, HIGH);     // enable pull-ups
   digitalWrite (PN1, HIGH);
-  attachInterrupt (digitalPinToInterrupt(PN0), isr, CHANGE); 
+  attachInterrupt (digitalPinToInterrupt(PN0), isr, CHANGE);
 
   // Push button for LCD background
   digitalWrite(PN2, HIGH);
-  digitalWrite(LEDPN, HIGH);  
-  attachInterrupt (digitalPinToInterrupt(PN2), isp, RISING);  
+  digitalWrite(LEDPN, HIGH);
+  attachInterrupt (digitalPinToInterrupt(PN2), isp, RISING);
 }
 
 const unsigned long lastPumpDtMin = (unsigned long) (1 * 60 * 1000L);   // the minimal intervall between two pumping actions
@@ -148,15 +148,15 @@ const int dt = 50;                                                      // ms de
 const int sensThrMax = 100;
 const int sensThrMin = 0;
 
-void loop() {  
+void loop() {
   static int sensThr = 50;
   static int sensorValueRaw = 0;        // value read from the pot
-  static int sensorValueMapped = 0;  
+  static int sensorValueMapped = 0;
   static unsigned long lastPumpCnt = (long) (lastPumpDtMin / dt) + 1;
-  static int pumpDurCnt = 0;  
+  static int pumpDurCnt = 0;
   static bool pumping = false;
   static unsigned long lastPushedCnt = 0;
-    
+
   // Updating the threshold via the encoder variable.
   if (turned)
     {
@@ -165,14 +165,14 @@ void loop() {
           sensThr--;
           delay(10);
         }
-      }else{          
-         if (sensThr < sensThrMax){ 
+      }else{
+         if (sensThr < sensThrMax){
           sensThr++;
           delay(10);
          }
-      } 
-    turned = false;           
-    digitalWrite(LEDPN, HIGH);    
+      }
+    turned = false;
+    digitalWrite(LEDPN, HIGH);
     pushed = false;
     lastPushedCnt = 0;
    }
@@ -187,48 +187,50 @@ void loop() {
   printLcd(sensorValueMapped, sensThr, pumping);
 
   // Checking and setting the pump activity.
-  if (sensorValueMapped < sensThr){      
+  if (sensorValueMapped < sensThr){
       // Too dry
       if (pumping){
         pumpDurCnt++;
         if (pumpDurCnt * dt > pumpDtMax){
-          digitalWrite(PUMPPN, LOW); 
+          digitalWrite(PUMPPN, LOW);
           pumping = false;
         }
       }else{
         if (lastPumpCnt * dt > lastPumpDtMin){
           lastPumpCnt = 0;
           pumpDurCnt = 0;
-          digitalWrite(PUMPPN, HIGH);  
-          pumping = true; 
+          digitalWrite(PUMPPN, HIGH);
+          pumping = true;
         }
-        lastPumpCnt++;    
-    }                
-  }else{    
+        lastPumpCnt++;
+    }
+  }else{
     // not too dry
     if (pumping){
       pumpDurCnt++;
-      if (pumpDurCnt * dt > pumpDtMax){            
-        digitalWrite(PUMPPN, LOW);   
-        pumping = false;        
+      if (pumpDurCnt * dt > pumpDtMax){
+        digitalWrite(PUMPPN, LOW);
+        pumping = false;
       }
     }else{
-      lastPumpCnt++;      
-    }    
+      lastPumpCnt++;
+    }
   }
 
-  // LCD LED background light
+  // This is where the MuFuK (MultiFunctionalKnob) will change the menu to
+  // adjust settings with the MuC (MultiCrementor) like threshold, watering time
+  // and the interval between two watering attempts
   if (pushed){
-    digitalWrite(LEDPN, HIGH);    
+    digitalWrite(LEDPN, HIGH);
     pushed = false;
     lastPushedCnt = 0;
   }else{
     lastPushedCnt++;
     if (lastPushedCnt * dt >= brightTime){
-      digitalWrite(LEDPN, LOW);    
+      digitalWrite(LEDPN, LOW);
     }
   }
-  
+
   delay(dt);
 }
 
@@ -240,41 +242,35 @@ void printLcd(int sensorValue, int threshold, bool pumping){
     return;
   lastSensorValue = sensorValue;
   lastThreshold = threshold;
-      
+
   String thresholdBaseString = "Threshold: ";
   String thresholdString = thresholdBaseString + threshold;
   String sensorBaseString = "Sensor: ";
-  String sensorString = sensorBaseString + sensorValue;  
-  
+  String sensorString = sensorBaseString + sensorValue;
+
   lcd.clear();
-  lcd.setCursor(0, 0);  
+  lcd.setCursor(0, 0);
   lcd.print(thresholdString);
-  lcd.setCursor(0, 1);  
+  lcd.setCursor(0, 1);
   lcd.print(sensorString);
   if (sensorValue < threshold){
-    lcd.setCursor(14, 0);  
+    lcd.setCursor(14, 0);
     lcd.write(byte(0));
-    lcd.setCursor(15, 0);  
+    lcd.setCursor(15, 0);
     lcd.write(byte(1));
-    lcd.setCursor(14, 1);  
+    lcd.setCursor(14, 1);
     lcd.write(byte(2));
-    lcd.setCursor(15, 1);  
+    lcd.setCursor(15, 1);
     lcd.write(byte(3));
   }else{
-    lcd.setCursor(14, 0);  
+    lcd.setCursor(14, 0);
     lcd.write(byte(4));
-    lcd.setCursor(15, 0);  
+    lcd.setCursor(15, 0);
     lcd.write(byte(5));
-    lcd.setCursor(14, 1);  
+    lcd.setCursor(14, 1);
     lcd.write(byte(6));
-    lcd.setCursor(15, 1);  
+    lcd.setCursor(15, 1);
     lcd.write(byte(7));
   }
-  
+
 }
-
-
-
-
-
-
