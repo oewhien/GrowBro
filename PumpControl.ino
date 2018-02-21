78ee62f78a2d9bd26ec108c2a07f6b66438da016
Erik Bernitt <erik@Erik-MBA.local>
2018-02-21 22:23:21 +0100

    Implemented MuC & MuFuK concept for editing pump duration and intervals.

--- PumpControl.ino
+++ PumpControl.ino
@@ -1,5 +1,5 @@
 #include <LiquidCrystal.h>
-
+#include "includeFile.h"
 // LCD
 #define RS 12
 #define EN 11
@@ -13,93 +13,16 @@
 #define ANINPN A0
 #define MESVPN 13
 // Button and rotation encoder
-#define PN0 2 // This is the MuC
-#define PN1 4 // This is also the MuC
-#define PN2 3 // This is the MuFuK
+#define PN0 2 // Muc
+#define PN1 4 // Muc
+#define PN2 3 // MuFuK
+#define DBNC 20 //Debounce time
 
 LiquidCrystal lcd(RS, EN, D4, D5, D6, D7);
 
 volatile boolean up;
 volatile boolean turned;
 volatile boolean pushed = true;
-
-
-// Custom characters for LCD
-byte canUl[8] = {
-  B00001,
-  B00010,
-  B00100,
-  B00101,
-  B00011,
-  B00001,
-  B01100,
-};
-byte canUr[8] = {
-  B10000,
-  B01000,
-  B11100,
-  B11110,
-  B11111,
-  B11111,
-  B11111,
-};
-byte canLl[8] = {
-  B00111,
-  B10000,
-  B00000,
-  B01000,
-  B00000,
-  B00100,
-  B10000,
-};
-byte canLr[8] = {
-  B11110,
-  B01100,
-  B00000,
-  B00000,
-  B00000,
-  B00000,
-  B00000,
-};
-
-byte flowerUl[8] = {
-  B00000,
-  B00000,
-  B00111,
-  B01100,
-  B11000,
-  B10010,
-  B10000,
-};
-byte flowerUr[8] = {
-  B00000,
-  B00000,
-  B11100,
-  B00110,
-  B00011,
-  B01001,
-  B00001,
-};
-byte flowerLl[8] = {
-  B10000,
-  B10010,
-  B11001,
-  B01100,
-  B00111,
-  B00000,
-  B00000,
-};
-byte flowerLr[8] = {
-  B00001,
-  B01001,
-  B10011,
-  B00110,
-  B11100,
-  B00000,
-  B00000,
-};
-
-
 
 // Interrupt Service Routine for a change to encoder pin
 void isr ()
@@ -141,34 +64,80 @@ void setup() {
   attachInterrupt (digitalPinToInterrupt(PN2), isp, RISING);
 }
 
-const unsigned long lastPumpDtMin = (unsigned long) (1 * 60 * 1000L);   // the minimal intervall between two pumping actions
-const int pumpDtMax = (int) (5 * 1000);                                 // the maximal pumping action duration
 const unsigned long brightTime = 1 * 60 * 1000L;                        // the time the LCD is lit after button / rotation knob action
-const int dt = 50;                                                      // ms delay time of loop
-const int sensThrMax = 100;
-const int sensThrMin = 0;
+const unsigned int dt = 50;                                             // ms delay time of loop
+const unsigned int sensThrMax = 100;
+const unsigned int sensThrMin = 0;
+const unsigned long pumpDtMax = (1 * 60 * 1000L);
+const unsigned int pumpDtMin = (int) (5 * 1000);
+const unsigned long interPumpDtMax = (2L * 24L * 60L * 60L * 1000L);
+const unsigned long interPumpDtMin = (1 * 60 * 60 * 1000L);
 
 void loop() {
-  static int sensThr = 50;
-  static int sensorValueRaw = 0;        // value read from the pot
-  static int sensorValueMapped = 0;
-  static unsigned long lastPumpCnt = (long) (lastPumpDtMin / dt) + 1;
-  static int pumpDurCnt = 0;
+  static unsigned int sensThr = 50;
+  static unsigned long pumpDt = (int) (10 * 1000);                           // the maximal pumping action duration
+  static unsigned long interPumpDt = (unsigned long) (2 * 60 * 60 * 1000L);      // the minimal intervall between two pumping actions
+  static unsigned int sensorValueRaw = 0;                                   // value read from the pot
+  static unsigned int sensorValueMapped = 0;
+  static unsigned long lastPumpCnt = (long) (interPumpDt / dt) + 1;
+  static unsigned int pumpDurCnt = 0;
   static bool pumping = false;
   static unsigned long lastPushedCnt = 0;
+  static MUFUKSTATE mufState;
 
-  // Updating the threshold via the encoder variable.
+
+  // MuFuK: LCD LED background light and value selection
+  if (pushed){
+    delay(DBNC);   // debounce
+    digitalWrite(LEDPN, HIGH);
+    pushed = false;
+    if (lastPushedCnt * dt < brightTime){
+      mufState = MUFUKSTATE((((int) mufState) + 1) % 3);    // Is that really okay?
+    }
+    lastPushedCnt = 0;
+    }else{
+      lastPushedCnt++;
+      if (lastPushedCnt * dt >= brightTime){
+        digitalWrite(LEDPN, LOW);
+        mufState = MUFUKSTATE(0);
+      }
+    }
+
+  // MuC:Updating the threshold via the encoder variable.
   if (turned)
     {
       if (up){
-        if (sensThr > sensThrMin){
-          sensThr--;
-          delay(10);
+        switch(mufState){
+          case thrSel:
+            if (sensThr > sensThrMin)
+              sensThr--;
+            break;
+            case pumpDtSel:
+              if (pumpDt > pumpDtMin)
+                pumpDt -= 1000;
+              break;
+            case wateringDtSel:
+              if (interPumpDt > interPumpDtMin)
+                interPumpDt -= 3600000L;
+              break;
         }
+        delay(DBNC);     // debounce
       }else{
-         if (sensThr < sensThrMax){
-          sensThr++;
-          delay(10);
+        switch(mufState){
+          case thrSel:
+            if (sensThr < sensThrMax)
+              sensThr++;
+            break;
+           case pumpDtSel:
+            if (pumpDt < pumpDtMax)
+              pumpDt+= 1000;
+            break;
+            case wateringDtSel:
+              if (interPumpDt < interPumpDtMax)
+                interPumpDt += 3600000L;
+              break;
+
+          delay(DBNC);   // debounce
          }
       }
     turned = false;
@@ -184,19 +153,31 @@ void loop() {
   delay(1);
   digitalWrite(MESVPN, LOW);
   sensorValueMapped = map(sensorValueRaw, 0, 1023, sensThrMin, sensThrMax);
-  printLcd(sensorValueMapped, sensThr, pumping);
+
+
+  switch(mufState){
+    case thrSel:
+      printLcd(sensorValueMapped, sensThr, sensorValueMapped, sensThr, pumping, mufState);
+      break;
+    case pumpDtSel:
+      printLcd(sensorValueMapped, sensThr, 0, pumpDt, pumping, mufState);
+      break;
+    case wateringDtSel:
+      printLcd(sensorValueMapped, sensThr, 0, interPumpDt, pumping, mufState);
+      break;
+  }
 
   // Checking and setting the pump activity.
   if (sensorValueMapped < sensThr){
       // Too dry
       if (pumping){
         pumpDurCnt++;
-        if (pumpDurCnt * dt > pumpDtMax){
+        if (pumpDurCnt * dt > pumpDt){
           digitalWrite(PUMPPN, LOW);
           pumping = false;
         }
       }else{
-        if (lastPumpCnt * dt > lastPumpDtMin){
+        if (lastPumpCnt * dt > interPumpDt){
           lastPumpCnt = 0;
           pumpDurCnt = 0;
           digitalWrite(PUMPPN, HIGH);
@@ -208,7 +189,7 @@ void loop() {
     // not too dry
     if (pumping){
       pumpDurCnt++;
-      if (pumpDurCnt * dt > pumpDtMax){
+      if (pumpDurCnt * dt > pumpDt){
         digitalWrite(PUMPPN, LOW);
         pumping = false;
       }
@@ -217,41 +198,50 @@ void loop() {
     }
   }
 
-  // LCD LED background light
-  if (pushed){
-    digitalWrite(LEDPN, HIGH);
-    pushed = false;
-    lastPushedCnt = 0;
-  }else{
-    lastPushedCnt++;
-    if (lastPushedCnt * dt >= brightTime){
-      digitalWrite(LEDPN, LOW);
-    }
-  }
-
   delay(dt);
 }
 
 
-void printLcd(int sensorValue, int threshold, bool pumping){
-  static int lastSensorValue;
-  static int lastThreshold;
-  if (sensorValue == lastSensorValue && threshold == lastThreshold)
-    return;
-  lastSensorValue = sensorValue;
-  lastThreshold = threshold;
 
-  String thresholdBaseString = "Threshold: ";
-  String thresholdString = thresholdBaseString + threshold;
-  String sensorBaseString = "Sensor: ";
-  String sensorString = sensorBaseString + sensorValue;
+void printLcd(int isHum, int humThr, int isVal, unsigned long editVal, bool pumping, MUFUKSTATE state){
+  static int lastIsVal;
+  static int lastThr;
+  static MUFUKSTATE lastState;
+  if (isVal == lastIsVal && editVal == lastThr && lastState == state)
+    return;
+
+  lastIsVal = isVal;
+  lastThr = editVal;
+  lastState = state;
+
+  String topRowBaseStr = "";
+  String lowRowBaseStr = "";
+  String topRowStr = "";
+  String lowRowStr = "";
+  switch (state){
+    case thrSel:
+      topRowBaseStr = "Threshold: ";
+      topRowStr = topRowBaseStr + editVal;
+      lowRowBaseStr = "Sensor: ";
+      lowRowStr = lowRowBaseStr + isVal;
+      break;
+    case pumpDtSel:
+      topRowStr = "Pump duration: ";
+      lowRowStr = lowRowBaseStr + (editVal/1000L) + " s";
+      break;
+    case wateringDtSel:
+      topRowStr = "Pump interval ";
+      lowRowStr = lowRowBaseStr + (editVal/(3600000L)) + " h";
+      break;
+  }
+
 
   lcd.clear();
   lcd.setCursor(0, 0);
-  lcd.print(thresholdString);
+  lcd.print(topRowStr);
   lcd.setCursor(0, 1);
-  lcd.print(sensorString);
-  if (sensorValue < threshold){
+  lcd.print(lowRowStr);
+  if (isHum < humThr){
     lcd.setCursor(14, 0);
     lcd.write(byte(0));
     lcd.setCursor(15, 0);
